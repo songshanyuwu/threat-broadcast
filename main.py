1126b1db6a30f7905883b3ad3c66ab64304d0e13
songshanyuwu <songzhijunpro@163.com>
2021-12-21 17:09:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -66,9 +66,9 @@ def main(help, top, auto_commit, gtk, mail_smtp, mail_user, mail_pass, qq_user, 
 
         if all_cves:
             page.to_page(top)
-            # mail.to_mail(gtk, all_cves, mail_smtp, mail_user, mail_pass)
-            # qq.to_group(all_cves, qq_user, qq_pass)
-            # wechat.to_wechat(all_cves)
+            mail.to_mail(gtk, all_cves, mail_smtp, mail_user, mail_pass)
+            qq.to_group(all_cves, qq_user, qq_pass)
+            wechat.to_wechat(all_cves)
             pushplus.to_pushplus(all_cves)
 
             if auto_commit:
