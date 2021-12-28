bb10bb947332e362e92a5d3c6d0dd571b7d270f4
songshanyuwu <songzhijunpro@163.com>
2021-12-28 08:56:42 +0800

    Update pushplus.py

--- src/notice/pushplus.py
+++ src/notice/pushplus.py
@@ -49,11 +49,12 @@ def format_content(cves):
 <h3>详细漏洞清单如下：</h3>
 <br/>
 %(cve_infos)s
-
-<br/><br/>
-++++++++++++++++++++++++++++++++++++++++++++++
 <br/>
-<font color="red">【情报收集与播报支持】</font> <a href="https://lyy289065406.github.io/threat-broadcast/">https://lyy289065406.github.io/threat-broadcast/</a>
++++++++++++++++++++++++++++++++++
+<br/>
+<font color="red">【情报收集与播报支持】</font>
+<br/>
+<a href="https://lyy289065406.github.io/threat-broadcast/">https://lyy289065406.github.io/threat-broadcast/</a>
 '''
     src_infos = []
     cve_infos = []
